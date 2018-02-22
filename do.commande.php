183d6f797079bc9de18ffa76f9d143d567318d21
MarineLohezic <lohezic.marine@gmail.com>
2018-02-22 16:21:29 +0100

    resultat.php google map

--- do.commande.php
+++ do.commande.php
@@ -19,25 +19,28 @@ $dateFrom =date("d/m/Y", strtotime($_POST['date_from']));
 	curl_setopt($connexionFile, CURLOPT_URL, "http://exapaq.pickup-services.com/Exapaq/mypudofull.asmx/GetPudoList?address=".$_POST['address']."&zipCode=".$_POST['ZIPCode']."&city=".$_POST['city']."&request_id=".time()."&date_from=".$dateFrom);
 	// Exec
 	curl_exec($connexionFile);
+	//Error
 	// Close
 	curl_close($connexionFile);
-	echo("File created: <a href='.\output.html'> 
-					file </a>");
-
+	echo("File created: <a href='.\output.html'> file </a>");
 
 	// init()
-	$connexion = curl_init();
+	//$connexion = curl_init();
 	//opt
-	curl_setopt($connexion, CURLOPT_URL, "/Exapaq/mypudofull.asmx/GetPudoList?address=".$_POST['address']."&zipCode=".$_POST['ZIPCode']."&city=".$_POST['city']."&request_id=".time()."&date_from=".$_POST['date_from']);
+	/*curl_setopt($connexion, CURLOPT_URL, "/Exapaq/mypudofull.asmx/GetPudoList?address=".$_POST['address']."&zipCode=".$_POST['ZIPCode']."&city=".$_POST['city']."&request_id=".time()."&date_from=".$_POST['date_from']);
 	curl_setopt($connexion, CURLOPT_HTTPGET, true);
 	curl_setopt($connexion, CURLOPT_RETURNTRANSFER, true);
 	// get in var
-	$data = curl_exec($connexion);
+	$data = curl_exec($connexion);*/
 	// close connexion
-	curl_close($connexion);
-	echo("data: ". $data);
+	/*print_r(curl_error($connexion));*/
+
+	//curl_close($connexion);
+	//echo("data: ". $data);
 	echo(time());
 	echo($_POST['address']." ".$_POST['ZIPCode']." ".$_POST['city']." ".time()." ".$dateFrom);
+
+	
 // Envoyer la requete au serveur
 
 // Enregistrer le fichier
--- resultat.php
+++ resultat.php
@@ -6,7 +6,39 @@
         <title>Resultat</title>
     </head>
     <body>
-        <div class="colonne droite">
+        <div class="row">
+            <div class="colonne">
+                <input id="checkBox" type="checkbox">
+            </div>
+            <div class="colonne">
+                <div id="map"></div>
+                <div class="row space-evenly">
+                    <div class="colonne">
+                        <p> Nom : </p>
+                        <p> Adresse :</p>
+                    </div>
+                    <div class="colonne">
+                        <p class="bold" > Heures d'ouverture :</p>                        
+                    </div>
+                </div>                
+            </div>
         </div>
+        <script>
+          var map;
+          function initMap() {
+            map = new google.maps.Map(document.getElementById('map'), {
+              center: {lat: -34.397, lng: 150.644},
+              zoom: 8
+            });
+          }
+
+          function test(){
+            console.log("test");
+          }
+        </script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC-tcIo3j9FrePCRwAb5VSRHQ_IUBkiMf8&callback=initMap" async defer></script>
+
+        <!-- <script src="https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyD8LZnDRxHPwBpGU3oiCaBekDgxjYNyCbw" async defer></script> -->
+            
     </body>
 </html>
