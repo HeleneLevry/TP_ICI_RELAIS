c7193cb3c8edb8c9d873fa0c2928ae2361562c58
HeleneLevry <helenelevry@gmail.com>
2018-02-22 15:19:37 +0100

    request working

--- do.commande.php
+++ do.commande.php
@@ -6,8 +6,44 @@
 
 // ----- Treatment -----
 
-$dateFrom =date("d/m/Y", strtotime($_POST['date_from']));
-// output declaration
+if (parameterControl()){
+	// Enregistrer le fichier
+	recordInFile();
+	// Envoyer la requete au serveur
+	$data = getData();
+	echo $data;
+}
+else{
+	echo '<p>Some fields are missing</p>';
+	exit();
+}
+
+
+// Utiliser le flux XML pour creation d'un tableau
+
+// ----- Redirection -----
+
+// --------------------  --------------------
+// -------------------- FUNCTIONS --------------------
+
+// ----- Treatment -----
+// parameterControl
+function parameterControl(){
+	if ( isset($_POST["address"]) AND isset($_POST["ZIPCode"])  AND isset($_POST["city"])  AND isset($_POST["date_from"]) ) {
+		global $dateFrom;
+		$_POST['date_from'] =date("d/m/Y", strtotime($_POST['date_from']));
+		return true;
+	}
+	else {
+		return false;
+	}	
+}
+// recordInFile()
+function recordInFile(){
+	// URL
+	$URL = "http://exapaq.pickup-services.com/Exapaq/mypudofull.asmx/GetPudoList?address=".$_POST['address']."&ZIPCode=".$_POST['ZIPCode']."&city=".$_POST['city']."&request_id=".time()."&date_from=".$_POST['date_from'];
+	$URL = str_replace(" ", '%20', $URL);
+	// output declaration
 	$outputFile = "./output.xml";
 	// open file
 	$fp = fopen($outputFile, "w");
@@ -16,41 +52,35 @@ $dateFrom =date("d/m/Y", strtotime($_POST['date_from']));
 	// setOPT
 	curl_setopt($connexionFile, CURLOPT_FILE, $fp);
 	curl_setopt($connexionFile, CURLOPT_HTTPGET, true);
-	curl_setopt($connexionFile, CURLOPT_URL, "http://exapaq.pickup-services.com/Exapaq/mypudofull.asmx/GetPudoList?address=".$_POST['address']."&zipCode=".$_POST['ZIPCode']."&city=".$_POST['city']."&request_id=".time()."&date_from=".$dateFrom);
+	curl_setopt($connexionFile, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+	curl_setopt($connexionFile, CURLOPT_URL, $URL);
 	// Exec
 	curl_exec($connexionFile);
 	// Close
 	curl_close($connexionFile);
-	echo("File created: <a href='.\output.html'> 
-					file </a>");
-
-
-	// init()
+}
+// getData();
+function getData(){
+	// URL
+	$URL = "http://exapaq.pickup-services.com/Exapaq/mypudofull.asmx/GetPudoList?address=".$_POST['address']."&ZIPCode=".$_POST['ZIPCode']."&city=".$_POST['city']."&request_id=".time()."&date_from=".$_POST['date_from'];
+	$URL = str_replace(" ", '%20', $URL);
+	// init curl connexion
 	$connexion = curl_init();
-	//opt
-	curl_setopt($connexion, CURLOPT_URL, "/Exapaq/mypudofull.asmx/GetPudoList?address=".$_POST['address']."&zipCode=".$_POST['ZIPCode']."&city=".$_POST['city']."&request_id=".time()."&date_from=".$_POST['date_from']);
+	// curl opt
 	curl_setopt($connexion, CURLOPT_HTTPGET, true);
+	curl_setopt($connexion, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
 	curl_setopt($connexion, CURLOPT_RETURNTRANSFER, true);
-	// get in var
+	curl_setopt($connexion, CURLOPT_URL, $URL);
+	// Execute curl 
 	$data = curl_exec($connexion);
-	// close connexion
+	// Close curl connexion
 	curl_close($connexion);
-	echo("data: ". $data);
-	echo(time());
-	echo($_POST['address']." ".$_POST['ZIPCode']." ".$_POST['city']." ".time()." ".$dateFrom);
-// Envoyer la requete au serveur
-
-// Enregistrer le fichier
-
-// Utiliser le flux XML pour creation d'un tableau
+	//return 
+	return $data;
+}
 
 // ----- Redirection -----
 
-// -------------------- FUNCTIONS --------------------
-
-// ----- Treatment -----
-
-// ----- Redirection -----
-
+// --------------------  --------------------
 
 ?>
