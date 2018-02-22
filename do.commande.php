96cd3808a698a95d3fdf40b42f7529ca1a49f1c0
HeleneLevry <helenelevry@gmail.com>
2018-02-22 14:34:44 +0100

    new request

--- do.commande.php
+++ do.commande.php
@@ -5,8 +5,39 @@
 // -------------------- PROGRAMME --------------------
 
 // ----- Treatment -----
-getInFile();
 
+$dateFrom =date("d/m/Y", strtotime($_POST['date_from']));
+// output declaration
+	$outputFile = "./output.xml";
+	// open file
+	$fp = fopen($outputFile, "w");
+	// init connection
+	$connexionFile = curl_init();
+	// setOPT
+	curl_setopt($connexionFile, CURLOPT_FILE, $fp);
+	curl_setopt($connexionFile, CURLOPT_HTTPGET, true);
+	curl_setopt($connexionFile, CURLOPT_URL, "http://exapaq.pickup-services.com/Exapaq/mypudofull.asmx/GetPudoList?address=".$_POST['address']."&zipCode=".$_POST['ZIPCode']."&city=".$_POST['city']."&request_id=".time()."&date_from=".$dateFrom);
+	// Exec
+	curl_exec($connexionFile);
+	// Close
+	curl_close($connexionFile);
+	echo("File created: <a href='.\output.html'> 
+					file </a>");
+
+
+	// init()
+	$connexion = curl_init();
+	//opt
+	curl_setopt($connexion, CURLOPT_URL, "/Exapaq/mypudofull.asmx/GetPudoList?address=".$_POST['address']."&zipCode=".$_POST['ZIPCode']."&city=".$_POST['city']."&request_id=".time()."&date_from=".$_POST['date_from']);
+	curl_setopt($connexion, CURLOPT_HTTPGET, true);
+	curl_setopt($connexion, CURLOPT_RETURNTRANSFER, true);
+	// get in var
+	$data = curl_exec($connexion);
+	// close connexion
+	curl_close($connexion);
+	echo("data: ". $data);
+	echo(time());
+	echo($_POST['address']." ".$_POST['ZIPCode']." ".$_POST['city']." ".time()." ".$dateFrom);
 // Envoyer la requete au serveur
 
 // Enregistrer le fichier
@@ -18,25 +49,6 @@ getInFile();
 // -------------------- FUNCTIONS --------------------
 
 // ----- Treatment -----
-// ----- Treatment -----
-// getInFile
-function getInFile(){
-	// output declaration
-	$outputFile = "./output.html";
-	// open file
-	$fp = fopen($outputFile, "w");
-	// init connection
-	$connexionFile = curl_init();
-	// setOPT
-	curl_setopt($connexionFile, CURLOPT_FILE, $fp);
-	curl_setopt($connexionFile, CURLOPT_URL, "http://exapaq.pickup-services.com/Exapaq/mypudofull.asmx?op=GetPudoList");
-	// Exec
-	curl_exec($connexionFile);
-	// Close
-	curl_close($connexionFile);
-	echo("File created: <a href='.\output.html'> 
-					file </a>");
-}
 
 // ----- Redirection -----
 
