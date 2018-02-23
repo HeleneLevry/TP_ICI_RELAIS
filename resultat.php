c44575cb147a08b9ca6d6b482b104c9064b97b5b
HeleneLevry <helenelevry@gmail.com>
2018-02-23 22:08:03 +0100

    finalisation

--- resultat.php
+++ resultat.php
@@ -8,57 +8,80 @@
 </head>
 
 <body>
+
+    <!-- Exploit xml -->
     <?php $result = createTable();?>
-    <div class="row">
-        <div class="colonne">
+
+    <div class="rowBody">
+
+        <div class="colonneBody col300">
+
             <form>
-                <div class="colonne">
-                    <p>Veuillez choisir votre point relais :</p>
-                    <?php
-                    // On crée le formulaire avec les ratios
-                    foreach ($result as $key => $value) { 
-                        echo'
-                        <input type="radio" id="'.$key.'" name="test"  value="'.$value[1].'" onclick="info('.$key.','.count($result).')">
-                        <label for="'.$key.'">'.$value[1].'</label>
-                        ';  
-                    };
-                    ?>
-                </div>
+                <fieldset>
+                    <legend>
+                        Veuillez choisir votre point relais :
+                    </legend>
+                    <div class="colonne">
+                        <?php
+                            // On crée le formulaire avec les radios
+                        foreach ($result as $key => $value) { 
+                            echo('
+                                <label for="'.$key.'">
+                                <input class ="radio" type="radio" id="'.$key.'" name="test"  value="'.$value[1].'" onclick="info('.$key.','.count($result).')">
+                                '.ucwords(strtolower($value[1])).'
+                                </label>
+                                ');  
+                        }
+                        ?>
+                    </div>
+                </fieldset>
             </form>
 
         </div>
-        <div class="colonne">
-            <div id="map"></div>
-            <div class="row space-evenly">
-                <?php
-                // on crée l'encart informations de chaque relais
-                foreach ($result as $key => $value) {
-                    echo('<div id="div'.$key.'" class="colonne center" style="display:none;">');
-                    echo('<div id="nom'.$key.'" class="bold">'.$value[1].' </div>');                             
-                    echo('<div id="adresse1'.$key.'" > '.$value[2].' </div>');
-                    echo('<div id="adresse2'.$key.'" > '.$value[3].' </div>');
-                    echo('<div id="adresse3'.$key.'" > '.$value[4].' </div>');
-                    echo('<div id="CP'.$key.'" >'.$value[5].' '.$value[6].' </div>');
-                    echo('</div>');
-                };
-                ?>            
+
+        <div class="colonneBody flexStart">
+
+            <div id="map" >
             </div>
-            <div class="colonne">
-                <p id="titre" class="bold center" style="display: none;" > Heures d'ouverture :</p>
+
+            <div class="row">
+                <?php
+                    // on crée l'encart informations de chaque relais
+                    foreach ($result as $key => $value) {
+                        echo('
+                            <div id="adresse'.$key.'" class="colonne divAdresse" style="display:none;">
+                                <hr>
+                                <div>
+                                    <h3 id="nom'.$key.'">'.ucwords(strtolower($value[1])).'</h3>
+                                    <p id="adresse1'.$key.'" > '.strtolower($value[2]).' </p>
+                                    <p id="adresse2'.$key.'" > '.strtolower($value[3]).' </p>
+                                    <p id="adresse3'.$key.'" > '.strtolower($value[4]).' </p>
+                                    <p id="CP'.$key.'" >'.$value[5].' '.ucwords(strtolower($value[6])).' </p>
+                                </div>
+                                <hr>
+                            </div>');
+                    };
+                ?>          
+            </div>
+
+            <h3 id="titre" style="display: none;" > Heures d'ouverture :</h3>
+            
+            <div class="row">
                 <?php
                 //On crée le planning d'ouverture de chaque relais 
-                $semaine = array("Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche");
+                $jour = array("Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche");
                 foreach ($result as $key => $value) {
-                    echo('<div class="row" style="display: none;" id="horraire'.$key.'">');
+                    echo('<div id="horaire'.$key.'" class="row" style="display: none;">');
                     $nombre=0;
                     $i=0;
                     foreach ($value[9] as $cle => $valeur) {                                    
                         if($cle%2==0){
-                            echo('<div class="colonne marge10">');
-                            echo('<div class="bold">'.$semaine[$i].' :</div> ');
+                            echo('
+                                <div class="colonneJour">
+                                    <h4>'.$jour[$i].' :</h4> ');
                             $i++;
                         }
-                        echo( '<div>'.$valeur[1].' '.$valeur[2].'</div>');
+                        echo('<p>'.$valeur[1].' - '.$valeur[2].'</p>');
                         if($cle%2!=0){
                             echo('</div>');
                         }
@@ -67,108 +90,118 @@
                     if($nombre%2!=0){
                         echo('</div>');  
                     }
-                    echo('</div>');                                
+                    echo('</div>');                               
                 };
                 ?>                    
             </div>
-        </div>                
-    </div>
-</div>
-<script>
-    function getQueryVariable(variable)
-    {
-     var query = window.location.search.substring(1);
-     var vars = query.split("&");
-     for (var i=0;i<vars.length;i++) {
-         var pair = vars[i].split("=");
-         if(pair[0] == variable){return pair[1];}
-     }
-     return(false);
-    }
 
- var map;
+        </div>
 
- function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: Number(getQueryVariable("lat")), lng: Number(getQueryVariable("lng"))},
-        zoom: 13
-    });
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC-tcIo3j9FrePCRwAb5VSRHQ_IUBkiMf8&callback=initMap" async defer></script>
 
-    // Récupération de $result
-    var locations = <?php echo json_encode($result)?>;
-    var locations = Object.keys(locations).map(function(k) { return locations[k] });
-
-     var contentString = '<div id="content">'+
-            '<div id="siteNotice">'+'coucou'
-            '</div>'+'</div>';
-
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
-    });
-
-    var infowindows= new Object();
-
-    locations.forEach(function(element,index) {
-        var lat= element[8][0].replace(',','.');
-        var lng= element[7][0].replace(',','.');
-        var markerPosition = {lat: parseFloat(lat), lng: parseFloat(lng)};
-
-        var infowindow = new google.maps.InfoWindow({
-            content: '<div id="content">'+
-            '<div id="siteNotice">'+element[1][0]+ '</div>'+'</div>'
-        });
-        infowindows[element[1][0]]=infowindow;
-
-        var marker = new google.maps.Marker({
-            position : markerPosition,
-            title: element[1][0],
-            map: map
-        });
-        // aJout de l'evenement sur le bouton ratio correspondant
-        document.getElementById(index).addEventListener("click", (function() {
-            infowindows[element[1][0]].open(map, marker);
-        }));
-    });
-        //Marquer de position du client
-    var markerPosition = {lat: Number(getQueryVariable("lat")), lng: Number(getQueryVariable("lng"))};
-    var marker = new google.maps.Marker({
-        position : markerPosition,
-        title: "Ma position",
-        map: map
-    });
-}
-
-</script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC-tcIo3j9FrePCRwAb5VSRHQ_IUBkiMf8&callback=initMap" async defer></script>
-
-<script>
-    function info(num,max){
-        var i=0;
-        var titre = document.getElementById("titre");
-        titre.style="display:flex";
-        for(i=0;i<max;i++){
-            var heure = document.getElementById("horraire"+i);
-            var div = document.getElementById("div"+i);
-            if (i==num){
-                div.style="display:flex;";
-                heure.style="display:flex;";
-            }else{
-                div.style="display:none;";
-                heure.style="display:none;";
+        <script>
+        //-- Global variables
+        var map;
+        //-- getQueryVariable
+        function getQueryVariable(parameterName) {
+            // Get URL
+            var query = window.location.search.substring(1);
+            // Record by spliting on &
+            var vars = query.split("&");
+            for (var i=0;i<vars.length;i++) {
+                // Record by splitting on =
+                var pair = vars[i].split("=");
+                // return value of parameter "parameterName"
+                if(pair[0] == parameterName){
+                    return pair[1];
+                }
+            }
+            return false;
+        }
+        //-- initMap
+        function initMap() {
+            // Create map
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: Number(getQueryVariable("lat")), lng: Number(getQueryVariable("lng"))},
+                zoom: 13
+            });
+            // Récupération de $result by json to javascript variable
+            var locations = <?php echo json_encode($result)?>;
+            var locations = Object.keys(locations).map(function(k){ return locations[k] 
+            });
+            // Create object of infos to show
+            var infowindows= new Object();
+            // Create group of marker to center
+            var bounds = new google.maps.LatLngBounds();
+            // Get lat and lng for each result (and replace 1,0 by 1.0)
+            locations.forEach(function(element,index){
+                // Get lat and lng for each result (and replace 1,0 by 1.0)
+                var lat= element[8][0].replace(',','.');
+                var lng= element[7][0].replace(',','.');
+                // Record position of the marker for each result
+                var markerPosition = {lat: parseFloat(lat), lng: parseFloat(lng)};
+                // Create info to open with name of the result for each marker
+                var infowindow = new google.maps.InfoWindow({
+                    content: '<div id="content">'+
+                    '<div id="siteNotice">'+element[1][0]+ '</div>'+'</div>'
+                });
+                infowindows[element[1][0]]=infowindow;
+                // Create marker for each result
+                var marker = new google.maps.Marker({
+                    position : markerPosition,
+                    title: element[1][0],
+                    map: map
+                });
+                // add to group of marker to center
+                bounds.extend(marker.position);
+                // Ajout de l'evenement sur le bouton ratio correspondant
+                document.getElementById(index).addEventListener(
+                    "click", (function() {
+                        infowindows[element[1][0]].open(map, marker);
+                    })
+                );
+            });
+            // Marqueur de position du client
+            var markerPosition = {lat: Number(getQueryVariable("lat")), lng: Number(getQueryVariable("lng"))};
+            var marker = new google.maps.Marker({
+                position : markerPosition,
+                title: "Ma position",
+                map: map
+            });
+            // add to group of marker to center
+            bounds.extend(marker.position);
+            // auto-zoom
+            map.fitBounds(bounds);
+            // auto-center
+            map.panToBounds(bounds);
+        }
+        //-- info
+        function info(num,max){
+            // Afficher la section des heures d'ouverture
+            var titre = document.getElementById("titre");
+            titre.style="display:flex";
+            for(var i=0;i<max;i++){
+                // Element adresse et element horaire
+                var adresse = document.getElementById("adresse"+i);
+                var heure = document.getElementById("horaire"+i);
+                // afficher adresse et heure du result i, cacher les autres      
+                if (i==num){
+                    adresse.style="display:flex;";
+                    heure.style="display:flex;";
+                }else{
+                    adresse.style="display:none;";
+                    heure.style="display:none;";
+                }
             }
         }
-    }
-</script>
+    </script>
+
+</div>
 </body>
 </html>
 
 <?php
-
-function test(){
-    return $result[1][7];
-}
 // -------------------- FUNCTIONS --------------------
-
 // ----- Treatment -----
 // createTable
 function createTable(){
@@ -206,29 +239,5 @@ function createTable(){
     }
     return $result;
 }
-// findCoord
-function findCoord(){
-    global $result;
-    $tableCoord = array();
-    for($i=0; $i<count($result); $i++) {
-        $coordinates = array(str_replace(',','.',$result[$i][7][0]),str_replace(',','.',$result[$i][8][0]));
-        //print_r($result[$i][7][0]);
-        array_push($tableCoord, $coordinates);
-    }
-    foreach ($tableCoord as $coords) {
-        ?>
-        <script>
-            var lat = <?php echo $tableCoord[0]; ?>;
-            var long = <?php echo $tableCoord[1]; ?>;
-            var markerPosition = {lat: lat, lng: lng};
-            var marker = new google.maps.Marker({
-                position : markerPosition,
-                map: map
-            });
-        </script>
-    <?php
-    }
-}
-
 // --------------------  --------------------
 ?>
